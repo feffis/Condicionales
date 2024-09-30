bf62e6da557a85a4d11c2a1dc30db43196ca1e57
stefania sanchez <stefani2019sanchez@gmail.com>
2024-09-30 15:39:44 -0500

    Añadió detalles

--- sketch.js
+++ sketch.js
@@ -1,8 +1,6 @@
-function septun() {
+function septup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
 }
 
-function draw() {
-  background(10, random(0, 100), 255, 10);
-}
+function draw() {}
